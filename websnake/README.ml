eaae88d159408ec91666d530e68564f202085e5e
Richard Davison <richard.neil.davison@gmail.com>
2017-06-15 02:16:18 +0000

    Update README

---
+++ websnake/README.ml
@@ -0,0 +1,14 @@
+# ReasonReact Snake!
+
+Made with [ReasonReact](https://github.com/reasonml/reason-react), whose docs are [here](https://reasonml.github.io/reason-react/).
+
+```
+git clone https://github.com/rdavison/llama-snake
+cd llama-snake/websnake
+npm install
+npm start
+# in another tab
+npm run build
+```
+
+After you see the webpack compilation succeed (the `npm run build` step), open up the nested html files in `src/*` (**no server needed!**). Then modify whichever file in `src` and refresh the page to see the changes.
